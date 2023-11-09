04e181c5d4f7f5ce5c7b6fbc178f77ba0c00f023
igor-sterniczuk-vinci <130989091+igor-sterniczuk-vinci@users.noreply.github.com>
2023-11-09 18:25:40 +0100

    feat: test for greet a person with capital letters only

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 function isEmptyNullOrWhitespace(name) {
-  return name === null || name === undefined || name === '';
+  return name === null || name === undefined || name === "";
 }
+
+
 
 function greeting(name) {
   if (isEmptyNullOrWhitespace(name)) {
-    return 'Hello, my friend.';
+    return "Hello, my friend.";
   }
   return `Hello, ${name}.`;
 }
