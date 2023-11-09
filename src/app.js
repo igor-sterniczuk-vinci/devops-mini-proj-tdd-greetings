9069f1c60defd46d179901698cd3db7ce2aae3e0
hubert-lopaciuk-vinci <hubert.lopaciuk@student.vinci.be>
2023-11-09 18:12:11 +0100

    feat:greet a person with null, undefined and whitespace using default values

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
+function isEmptyNullOrWhitespace(name) {
+  return name === null || name === undefined || name === '';
+}
+
 function greeting(name) {
+  if (isEmptyNullOrWhitespace(name)) {
+    return 'Hello, my friend.';
+  }
   return `Hello, ${name}.`;
 }
 
