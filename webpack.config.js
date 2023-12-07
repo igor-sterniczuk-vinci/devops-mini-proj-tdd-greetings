51d38f7f4077723aa93c6c68fedf743dd8619186
igor-sterniczuk-vinci <130989091+igor-sterniczuk-vinci@users.noreply.github.com>
2023-12-07 21:49:01 +0100

    fix: lint rules fix + change to production mode on build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development", // ou 'production'
+  mode: "production", // ou 'production'
   entry: "./src/app.js",
   output: {
     filename: "bundle.js",
